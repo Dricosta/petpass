10af1cd21b210bdff5b2fbfb9463f2bfb6cb8f59
Adriano Costa <adrianocosta@Adrianos-MacBook-Air.local>
2019-03-28 13:13:32 -0300

    refactoring the landpage

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,23 +1,44 @@
 import React from 'react';
-import logo from '../../assets/petpass.png';
-import Button from '@material-ui/core/Button';
+import logoPetpass  from '../../assets/petpass.png';
 import './navbar.scss';
 
 class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false
+    }
+    this.OpenMenuHamburguer = this.OpenMenuHamburguer.bind(this);
+  }
+
+  OpenMenuHamburguer() {
+    return this.setState(state => ({
+      menuOpen: !state.menuOpen
+    }));
+  }
+
+
   render() {
     return (
       <nav className="Navbar">
-        <img src={logo} alt="petpass"/>
-        <div className="Navbar__group-button">
-        <Button variant="contained" color="primary" className="btn-navbar">
-          Sobre nós
-        </Button>
-        <Button variant="contained" color="primary" className="btn-navbar">
-          Cadastre-se
-        </Button>
-        <Button variant="contained" color="primary" className="btn-navbar">
-          Login
-        </Button>
+        <div className="Navbar_container">
+          <img className="Navbar_logo" src={logoPetpass} alt="petpass"/>
+          <div className={`Navbar_menu-hamburguer ${this.state.menuOpen ? 'open' : 'close'}`} onClick={this.OpenMenuHamburguer}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          {this.state.menuOpen || window.innerWidth > 768 ? 
+          <div className="Navbar_group-button">
+            <ul className="Navbar_group-button_content">
+              <li><a href="www.google.com">Sobre nós</a></li>
+              <li><a href="www.google.com">Cadastre-se</a></li>
+              <li><a href="www.google.com">Login</a></li>
+            </ul>
+          </div>
+          :
+          false
+          }
         </div>
       </nav>
     );
