75f6867e9f4079b6168364b0c48fdd5e9dde8501
Julio Cesar <julio.ckcalista@gmail.com>
2019-04-11 07:33:30 -0300

    register progress

--- src/components/UserRegister/Confirm.js
+++ src/components/UserRegister/Confirm.js
@@ -1,14 +1,12 @@
 import React, { Component, Fragment } from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText'
 import Button from '@material-ui/core/Button'
-
-/* import IconButton from '@material-ui/core/IconButton'
-import PhotoCamera from '@material-ui/icons/PhotoCamera' */
+import '../../reset.scss'
+import Navbar from '../Navbar'
+import Grid from '@material-ui/core/Grid'
+import Footer from '../Footer'
 
 export class Confirm extends Component {
     continue = e => {
@@ -27,41 +25,97 @@ export class Confirm extends Component {
             birthday, genre, bio, address, phone, accountNumber,
             agencyNumber, digit, pet: [{
                 petName, idNumber, petBio, petAvatar,
-                breed, weight, size }] }} = this.props
+                breed, weight, size }] } } = this.props
 
         return (
+            <div>
+                {/* Header */}
+                <Grid container>
+                    <Grid item xs={12}>
+                        <header style={styles.header}>
+                            <Navbar />
+                        </header>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <h1 style={styles.h1}>Confirme seus dados</h1>
+                    </Grid>
+                </Grid>
 
-            <Fragment>
-                <AppBar position="static" color="default">
-                    <Toolbar>
-                        <Typography variant="h6" color="inherit">
-                            Confirme seus dados
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
+                {/* Body */}
+                <Grid container direction='row' spacing={8} justify='center'>
+                    <Grid container direction='row' justify='center'>
+                        <Grid item xs={10} sm={8} container direction='row' justify='center'>
+                            <List component="nav">
+                                <ListItem>
+                                    <ListItemText primary="Email" secondary={email} />
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemText primary="Senha" secondary={password} />
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemText primary="Nome" secondary={name} />
+                                </ListItem>
+                            </List>
+                        </Grid>
 
-                <List component="nav">
-                    <ListItem>
-                        <ListItemText primary="Email" secondary={email} />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText primary="Nome" secondary={name} />
-                    </ListItem>
-                </List>
+                        <Grid item container justify='center' style={styles.footer}>
+                            <Grid item>
+                                <Button children='Voltar' color='primary' variant='outlined'
+                                    style={styles.button} onClick={this.back} />
+                            </Grid>
+                            <Grid item>
+                                <Button children='Confirmar e continuar' color='primary' variant='outlined'
+                                    style={styles.button} onClick={this.continue} />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
 
-                <Button children='Confirmar e continuar' color='primary' variant='outlined'
-                    style={styles.button} onClick={this.continue} />
-
-                <Button children='Voltar' color='primary' variant='outlined'
-                    style={styles.button} onClick={this.back} />
-            </Fragment>
+                <Footer />
+            </div>
         )
     }
 }
 
 const styles = {
+    header: {
+        backgroundColor: '#3d3029',
+        height: 76
+    },
+    h1: {
+        textAlign: 'center',
+        backgroundColor: '#3d3029',
+        color: '#fff',
+        fontWeight: 100,
+        padding: '20px 10px',
+        marginBottom: 30
+    },
+    formPage: {
+        paddingTop: 76,
+    },
+    banner: {
+        backgroundColor: '#d3d3d3',
+        margin: '0 10px',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        width: '100%'
+    },
+    formInput: {
+        width: '100%'
+    },
+    inputDate: {
+        margin: '12px 0'
+    },
+    radioGroup: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
     button: {
         margin: 15
+    },
+    footer: {
+        marginBottom: 30
     }
 }
 
--- src/components/UserRegister/PetDetails.js
+++ src/components/UserRegister/PetDetails.js
@@ -1,12 +1,17 @@
-import React, { Component, Fragment } from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import React, { Component } from 'react'
+import '../../reset.scss'
+import Navbar from '../Navbar'
+import Footer from '../Footer'
+import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
-
-/* import IconButton from '@material-ui/core/IconButton'
-import PhotoCamera from '@material-ui/icons/PhotoCamera' */
+import IconButton from '@material-ui/core/IconButton'
+import PhotoCamera from '@material-ui/icons/PhotoCamera'
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import OutlinedInput from '@material-ui/core/OutlinedInput'
 
 export class PetDetails extends Component {
     continue = e => {
@@ -21,73 +26,181 @@ export class PetDetails extends Component {
 
     render() {
         const { values, handleChange } = this.props
-        console.log(values.petName)
+
         return (
+            <div>
+                {/* Header */}
+                <Grid container>
+                    <Grid item xs={12}>
+                        <header style={styles.header}>
+                            <Navbar />
+                        </header>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <h1 style={styles.h1}>Cadastre seu pet</h1>
+                    </Grid>
+                </Grid>
 
-            <Fragment>
-                <AppBar position="static" color="default">
-                    <Toolbar>
-                        <Typography variant="h6" color="inherit">
-                            Cadastre seu pet
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
+                {/* Body */}
+                <Grid container direction='row' spacing={8} justify='center'>
+                    <Grid container direction='row' justify='center'>
+                        <Grid item xs={10} sm={8} container direction='row' justify='center'>
+                            <form style={styles.formInput}>
+                                <TextField
+                                    label="Nome"
+                                    margin="normal"
+                                    variant="outlined"
+                                    required={true}
+                                    fullWidth={true}
+                                    onChange={handleChange('petName')}
+                                    defaultValue={values.petName}
+                                />
 
-                <TextField label='Nome'
-                    type='text'
-                    variant='outlined'
-                    onChange={handleChange('')}
-                    defaultValue={''} />
+                                <TextField
+                                    label="Número de identificação"
+                                    margin="normal"
+                                    variant="outlined"
+                                    type='email'
+                                    required={true}
+                                    fullWidth={true}
+                                    onChange={handleChange('idNumber')}
+                                    defaultValue={values.idNumber}
+                                />
 
-                <TextField label='Número de identificação'
-                    type='text'
-                    variant='outlined'
-                    onChange={handleChange('')}
-                    defaultValue={''} /> <br />
+                                <Grid item container xs={12} sm={4} justify='center'>
+                                    <input accept="image/*" id="btn-petAvatar" type="file" hidden />
+                                    <label htmlFor="btn-petAvatar">Foto
+                                        <IconButton color="primary" component="span">
+                                            <PhotoCamera />
+                                        </IconButton>
+                                    </label>
+                                </Grid>
 
-                <TextField label='Descrição'
-                    type='text'
-                    variant='outlined'
-                    onChange={handleChange('')}
-                    defaultValue={''} />
+                                <TextField
+                                    label="Descrição"
+                                    margin="normal"
+                                    variant="outlined"
+                                    multiline
+                                    fullWidth={true}
+                                    onChange={handleChange('petBio')}
+                                    defaultValue={values.petBio}
+                                />
 
-                <TextField label='Foto'
-                    type='text'
-                    variant='outlined'
-                    onChange={handleChange('')}
-                    defaultValue={''} />
+                                <TextField
+                                    label="Raça"
+                                    margin="normal"
+                                    variant="outlined"
+                                    required={true}
+                                    fullWidth={true}
+                                    onChange={handleChange('breed')}
+                                    defaultValue={values.breed}
+                                />
 
-                <TextField label='Raça'
-                    type='text'
-                    variant='outlined'
-                    onChange={handleChange('')}
-                    defaultValue={''} />
+                                <TextField
+                                    label="Peso"
+                                    margin="normal"
+                                    variant="outlined"
+                                    required={true}
+                                    fullWidth={true}
+                                    onChange={handleChange('weight')}
+                                    defaultValue={values.weight} />
 
-                <TextField label='Peso'
-                    type='text'
-                    variant='outlined'
-                    onChange={handleChange('')}
-                    defaultValue={''} />
+                                <FormControl variant="outlined" style={styles.selectSize}>
+                                    <InputLabel
+                                        fullWidth={true}
+                                        ref={ref => {
+                                            this.InputLabelRef = ref;
+                                        }}
+                                        htmlFor="outlined-size"
+                                        style={styles.selectSize}>
+                                        Porte
+                                    </InputLabel>
+                                    <Select
+                                        fullWidth={true}
+                                        value={values.pet.size}
+                                        onChange={handleChange('size')}
+                                        input={
+                                            <OutlinedInput
+                                                labelWidth={100}
+                                                fullWidth={true}
+                                                name="size"
+                                                id="outlined-size"
+                                            />
+                                        }
+                                    >
+                                        <MenuItem value="">
+                                            
+                                        </MenuItem>
+                                        <MenuItem value={'small'}>Pequeno</MenuItem>
+                                        <MenuItem value={'medium'}>Médio</MenuItem>
+                                        <MenuItem value={'big'}>Grande</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </form>
+                        </Grid>
 
-                <TextField label='Porte'
-                    type='text'
-                    variant='outlined'
-                    onChange={handleChange('')}
-                    defaultValue={''} />
+                        <Grid item container justify='center' style={styles.footer}>
+                            <Grid item>
+                                <Button children='Voltar' color='primary' variant='outlined'
+                                    style={styles.button} onClick={this.back} />
+                            </Grid>
+                            <Grid item>
+                                <Button children='Continuar' color='primary' variant='outlined'
+                                    style={styles.button} onClick={this.continue} />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
 
-                <Button children='Continuar' color='primary' variant='outlined'
-                    style={styles.button} onClick={this.continue} />
-
-                <Button children='Voltar' color='primary' variant='outlined'
-                    style={styles.button} onClick={this.back} />
-            </Fragment>
+                {/* Footer */}
+                <Footer />
+            </div>
         )
     }
 }
 
 const styles = {
+    header: {
+        backgroundColor: '#3d3029',
+        height: 76
+    },
+    h1: {
+        textAlign: 'center',
+        backgroundColor: '#3d3029',
+        color: '#fff',
+        fontWeight: 100,
+        padding: '20px 10px',
+        marginBottom: 30
+    },
+    formPage: {
+        paddingTop: 76,
+    },
+    banner: {
+        backgroundColor: '#d3d3d3',
+        margin: '0 10px',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        width: '100%'
+    },
+    formInput: {
+        width: '100%'
+    },
+    inputDate: {
+        margin: '12px 0'
+    },
+    radioGroup: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
     button: {
         margin: 15
+    },
+    footer: {
+        marginBottom: 30
+    },
+    selectSize: {
+        width: '100%'
     }
 }
 
