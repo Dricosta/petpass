4a3e7b67b86eb887edb40395e1b209571ba4e275
Julio Cesar <julio.ckcalista@gmail.com>
2019-06-05 10:59:25 -0300

    services history progress...

--- src/components/ServiceHistory/InfoServices.js
+++ src/components/ServiceHistory/InfoServices.js
@@ -1,42 +1,80 @@
 import React, { Component } from 'react';
 import PerfilOwner from '../PerfilOwner/index';
-import Comments from '../Comments/index';
 import { distanceInWords } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import api from '../../services/api';
 import './style.scss'
+import { Grid, Button } from '@material-ui/core'
 
 let idLocalStorage = ''
-
-// Infos
-
-// nome Owner
-// foto Owner
-// email Owner
-// nome Jobber
-// foto Jobber
-// email Jobber
-// nomeAnimal
-// fotoAnimal
-// dataExecução
-// statusServico
-// statusPagamentoOwner
-// statusPagamentoJobber
 
 class InfoServices extends Component {
     constructor() {
         super()
         this.state = {
-            Comments: [{
-                _id: '123213',
-                photo: '',
-                rate: 5,
-                userName: 'Teste',
-                hour: distanceInWords(new Date(), { locale: pt }),
-                comment: 'This is a test'
+            services: [{
+                ownerName: 'Usuário Comum',
+                ownerEmail: 'user@comum.com',
+                jobberName: 'Usuário Prestador',
+                jobberEmail: 'user@prestador.com',
+                petName: 'Pet do Usuário',
+                execDate: '02/06/2019',
+                serviceStatus: 'Solicitado',
+                paymentOwner: 'Confirmar',
+                paymentJobber: 'Confirmar'
+            }, {
+                ownerName: 'Usuário Comum2',
+                ownerEmail: 'user@comum.com2',
+                jobberName: 'Usuário Prestador2',
+                jobberEmail: 'user@prestador.com2',
+                petName: 'Pet do Usuário2',
+                execDate: '02/06/20192',
+                serviceStatus: 'Solicitado2',
+                paymentOwner: 'Confirmar2',
+                paymentJobber: 'Confirmar2'
+            }, {
+                ownerName: 'Usuário Comum3',
+                ownerEmail: 'user@comum.com3',
+                jobberName: 'Usuário Prestador3',
+                jobberEmail: 'user@prestador.com3',
+                petName: 'Pet do Usuário3',
+                execDate: '03/06/30193',
+                serviceStatus: 'Solicitado3',
+                paymentOwner: 'Confirmar3',
+                paymentJobber: 'Confirmar3'
+            },{
+                ownerName: 'Usuário Comum3',
+                ownerEmail: 'user@comum.com3',
+                jobberName: 'Usuário Prestador3',
+                jobberEmail: 'user@prestador.com3',
+                petName: 'Pet do Usuário3',
+                execDate: '03/06/30193',
+                serviceStatus: 'Solicitado3',
+                paymentOwner: 'Confirmar3',
+                paymentJobber: 'Confirmar3'
+            },{
+                ownerName: 'Usuário Comum3',
+                ownerEmail: 'user@comum.com3',
+                jobberName: 'Usuário Prestador3',
+                jobberEmail: 'user@prestador.com3',
+                petName: 'Pet do Usuário3',
+                execDate: '03/06/30193',
+                serviceStatus: 'Solicitado3',
+                paymentOwner: 'Confirmar3',
+                paymentJobber: 'Confirmar3'
+            },{
+                ownerName: 'Usuário Comum3',
+                ownerEmail: 'user@comum.com3',
+                jobberName: 'Usuário Prestador3',
+                jobberEmail: 'user@prestador.com3',
+                petName: 'Pet do Usuário3',
+                execDate: '03/06/30193',
+                serviceStatus: 'Solicitado3',
+                paymentOwner: 'Confirmar3',
+                paymentJobber: 'Confirmar3'
             }],
-            UserLogado: [],
-            GenderUserLogado: ''
+            userLogado: [],
+            genderUserLogado: ''
         }
     }
 
@@ -45,15 +83,15 @@ class InfoServices extends Component {
     async componentDidMount() {
         idLocalStorage = localStorage.getItem("idOwner") || localStorage.getItem("idJobber")
         const responseUser = await api.get(`user/${idLocalStorage}`)
-        const responseComments = await api.get(`user/comments/${idLocalStorage}`)
+        const responseServices = await api.get(`user/services/${idLocalStorage}`)
 
-        console.log('comentarios:', responseComments)
+        console.log('serviços:', responseServices)
 
         console.log('usuario:', responseUser)
 
         this.setState({
-            UserLogado: responseUser.data.result,
-            Comments: responseComments.data.result
+            userLogado: responseUser.data.result,
+            services: responseServices.data.result
         }, () => {
             if (responseUser.data.result.gender === "h") {
                 this.setState({
@@ -68,43 +106,101 @@ class InfoServices extends Component {
     }
 
     render() {
-        const { UserLogado } = this.state;
+        const { userLogado } = this.state;
         const date = new Date();
         const birthDate = {
-            day: date.getDay(UserLogado.birthday),
-            month: date.getMonth(UserLogado.birthday),
-            year: date.getFullYear(UserLogado.birthday)
+            day: date.getDay(userLogado.birthday),
+            month: date.getMonth(userLogado.birthday),
+            year: date.getFullYear(userLogado.birthday)
         }
         return (
             <div className="InfoUser">
 
-                <PerfilOwner
-                    photo={UserLogado.photo}
-                    name={UserLogado.name}
-                    description={UserLogado.description}
-                    genero={this.state.GenderUserLogado}
+                {/* <PerfilOwner
+                    photo={userLogado.photo}
+                    name={userLogado.name}
+                    description={userLogado.description}
+                    genero={this.state.GenderuserLogado}
                     birthDate={`${birthDate.day}/${birthDate.month}/${birthDate.year}`}
-                    rate={UserLogado.rate}
-                    creditos={UserLogado.qtdCredit} />
+                    rate={userLogado.rate}
+                    creditos={userLogado.qtdCredit} /> */}
 
 
-                <div className="InfoUser_group">
-                    <div className="InfoUser_group-comments">
-                        {this.state.Comments.map((comments) => {
-                            return (<Comments
-                                key={comments._id}
-                                photo={comments.photo}
-                                rate={comments.rate}
-                                name={comments.userName}
-                                hour={distanceInWords(comments.date, new Date(), { locale: pt })}
-                                comments={comments.comment} />
+                <Grid container item xs={12} justify='flex-start'>
+                    <Grid item container xs={11} justify='center' style={styles.scroll}>
+                        {this.state.services.map((services) => {
+                            return (
+                                <Grid item container xs={12} key={services.petName} style={styles.serviceCard}
+                                    justify='space-between'>
+                                    <Grid item container xs={12} sm={4}>
+                                        <Grid item xs={12}>
+                                            <h3><strong>Usuário Dono</strong></h3>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <p><strong>Nome</strong>: {services.ownerName} </p>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <p><strong>Email</strong>: {services.ownerEmail}</p>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <p><strong>Pet</strong>: {services.petName}</p>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <p>{distanceInWords(services.execDate, new Date(), { locale: pt })} atrás</p>
+                                        </Grid>
+                                    </Grid>
+
+                                    <Grid item container xs={12} sm={3} style={styles.textCenter}>
+                                        <Grid item xs={12}>
+                                            <h3><strong>Status</strong></h3>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <p>{services.serviceStatus} </p>
+                                        </Grid>
+                                        <Grid item container xs={12} justify='space-around'>
+                                            <Grid item>
+                                                <Button style={styles.button}>{services.paymentOwner}</Button>
+                                            </Grid>
+                                            <Grid item>
+                                                <Button style={styles.button}>{services.paymentJobber}</Button>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+
+                                    <Grid item container xs={12} sm={4}>
+                                        <Grid item xs={12}>
+                                            <h3><strong>Prestador</strong></h3>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <p><strong>Nome</strong>: {services.jobberName} </p>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <p><strong>Email</strong>: {services.jobberEmail}</p>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
                             )
                         })}
-
-                    </div>
-                </div>
+                    </Grid>
+                </Grid>
             </div>
         );
+    }
+}
+
+const styles = {
+    serviceCard: {
+        marginBottom: '18px',
+        boxShadow: '0px 1px 5px 0px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 3px 1px -2px rgba(0,0,0,0.12)'
+    },
+    textCenter: {
+        textAlign: 'center'
+    },
+    button: {
+        textTransform: 'none'
+    },
+    scroll: {
+        scrollY: 'scroll'
     }
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL:'https://8d02a0dc.ngrok.io'});
+const api = axios.create({ baseURL:'https://283cc4e2.ngrok.io'});
 
 api.interceptors.request.use(async config => {
     const TOKEN_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoicHBhIiwiaWF0IjoxNTU4NzMyMzM0fQ.fATFFmy8k7QSToovOCsdHVf5cdJ5e3OLK26ppm8VoXw";
